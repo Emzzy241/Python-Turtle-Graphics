9d765c85665eb595967a17f25eed0d5fdaba3084
Emzzy241 <emzzyoluwole@gmail.com>
2024-09-20 23:12:57 +0100

    add circle shape

---
+++ circle.py
@@ -0,0 +1,9 @@
+# drawing a circle with the python module
+
+import turtle
+
+print("Drawing a circle with the python turtle module")
+print()
+turtle.bgcolor("brown")
+turtle.circle(100)
+turtle.done()
