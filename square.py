68511434bc1323236d0d25ca705b6b8f584f7f8e
Emzzy241 <emzzyoluwole@gmail.com>
2024-09-20 23:12:36 +0100

    add square shape

---
+++ square.py
@@ -0,0 +1,20 @@
+import turtle
+
+# Giving the app a background-color
+# The python turtle module is a popular way to introduce programming concepts and create graphics
+
+print("Drawing a Square with turtle module from python")
+turtle.bgcolor("brown")
+turtle.speed(1)
+
+
+# Actually drawing the square shape in python
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+
+turtle.done()
