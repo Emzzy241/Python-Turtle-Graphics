3b211284bc191155db232d3d8c4605e8b8a5b5ed
Emzzy241 <emzzyoluwole@gmail.com>
2024-09-22 18:15:16 +0100

    update shapes with parallelogram

---
+++ parallelogram.py
@@ -0,0 +1,18 @@
+import turtle
+
+
+print("Drawing a Parallelogram!")
+turtle.bgcolor("brown")
+turtle.speed(1)
+turtle.forward(180)
+turtle.left(50)
+turtle.forward(135)
+turtle.left(130)
+turtle.forward(350)
+turtle.left(50)
+turtle.forward(135)
+turtle.left(130)
+turtle.forward(180)
+
+
+turtle.done()
